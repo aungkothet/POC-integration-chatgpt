50856f19cfc5bad6f1f9ff775a724303b0f432c2
Aung Ko Thet <aungkothet@ucsy.edu.mm>
2024-06-03 22:54:09 +0700

    privacy-ploicy added

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,7 @@ const nextConfig = {
   env: {
     LINE_ACCESS_TOKEN: process.env.LINE_ACCESS_TOKEN,
     LINE_MESSAGING_API: process.env.LINE_MESSAGING_API
-  },
-  experimental: {
-    serverActions: true,
-  },
+  }
 }
 
 export default nextConfig
